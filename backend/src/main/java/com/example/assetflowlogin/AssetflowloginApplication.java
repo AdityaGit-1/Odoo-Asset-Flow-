02b5ada9f33990804e23f2ba8d1a15c8280e2cce
MilanMishra19 <mishramilan683@gmail.com>
2026-07-12 14:21:08 +0530

    added jwt service and properties

--- backend/src/main/java/com/example/assetflowlogin/AssetflowloginApplication.java
+++ backend/src/main/java/com/example/assetflowlogin/AssetflowloginApplication.java
@@ -2,8 +2,11 @@ package com.example.assetflowlogin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import com.example.assetflowlogin.security.jwt.JwtProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class AssetflowloginApplication {
 
 	public static void main(String[] args) {
