08543c267a3eb4aa5790d437c9cb505e6122b7ea
Shreyash-Sachit-Sahu <137154025+Shreyash-Sachit-Sahu@users.noreply.github.com>
2026-07-12 17:14:44 +0530

    Update CorsConfig.java

--- backend/src/main/java/com/example/assetflowlogin/config/CorsConfig.java
+++ backend/src/main/java/com/example/assetflowlogin/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.assetflow.assetflowlogin.config;
+package com.example.assetflowlogin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
